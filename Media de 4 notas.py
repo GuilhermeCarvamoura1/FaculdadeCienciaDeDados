99a56172fee1fe59a4ea43220cbeb579500b9fad
Guilherme Carvalho <99695253+GuilhermeCarvamoura1@users.noreply.github.com>
2024-03-14 19:24:16 -0300

    Add files via upload

---
+++ Media de 4 notas.py
@@ -0,0 +1,10 @@
+nota1= float(input("digite o primeiro numero:"))
+nota2= float(input("digite o segundo numero:"))
+nota3= float(input("digite o terceiro numero:"))
+nota4= float(input("digite o quarto numero:"))
+media = (nota1+nota2+nota3+nota4) / 4
+print("o resultado é igual:", media)
+if media >= 7:
+  print("aprovado")
+else:
+  print("reprovado")
