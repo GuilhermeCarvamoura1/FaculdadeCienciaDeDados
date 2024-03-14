1938086de292f406e77b45c0ea18dbea0f12c8c7
Guilherme Carvalho <99695253+GuilhermeCarvamoura1@users.noreply.github.com>
2024-03-14 19:37:57 -0300

    Add files via upload

---
+++ esboço de pagina de login.py
@@ -0,0 +1,19 @@
+print('------======Restaurante sabor bom======------')
+print('1 - cadastrar')
+print('2 - buscar')
+print('3 - sair')
+
+menu = int(input('digite o codigo da operação desejada:'))
+#um = para atribuir == para comparar
+if menu == 1:
+    print('vou cadastrar')
+elif menu == 2:
+    print('vou buscar')
+elif menu == 3:
+    print('vou sair')
+else:
+    print("opção invalida")
+
+
+
+
